0b1431c4e49ab7f1a835975ea483b5909b7a4bf7
marinaoleynik <tilda2003@gmail.com>
2017-03-13 12:17:10 +0200

    Links have been added to directory content. File read/write method has been changed.

--- src/main/java/it/discovery/marina/DownloaderServlet.java
+++ src/main/java/it/discovery/marina/DownloaderServlet.java
@@ -1,8 +1,7 @@
 package it.discovery.marina;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
+
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -10,7 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,36 +18,42 @@ import javax.servlet.http.HttpServletResponse;
 public class DownloaderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String PATH_ON_DRIVE = "C:\\base";
-	private static final int PAGE_SIZE = 20000;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException 
 	{
 			
 		Path newPath = Paths.get(PATH_ON_DRIVE, Optional.ofNullable(request.getPathInfo()).orElse(""));
 
 		if (Files.isDirectory(newPath)) 
 		{
-			showDirectoryContent(response, newPath);
+			showDirectoryContent(response, newPath, request.getRequestURL().toString());
 		}
 
 		if (Files.isRegularFile(newPath))
 		{
 			showFileContent(response, newPath);
+			
 		}
 	}
 
-	private void showDirectoryContent(HttpServletResponse response, Path newPath) {
-		
+	private void showDirectoryContent(HttpServletResponse response, Path newPath, String servletPath) 
+	{
+		response.setContentType("text/html");
 		try(PrintWriter writer = response.getWriter();)
 		{
-			writer.append("Directory Content: \n");
-		
+			
+			writer.append("Directory Content: <br/>");
 			DirectoryStream<Path> stream = Files.newDirectoryStream(newPath);
 			for (Path item : stream) {
 				if (Files.isDirectory(item)) {
-					writer.append("<Directory> ");
+					 writer.append("Directory ");
 				}
-				writer.append(item.getFileName().toString()).append("\n");
+				writer.append("<a href=\"");
+				writer.append(servletPath +"/"+item.getFileName());
+				writer.append("\">");
+				
+				writer.append(item.getFileName().toString());
+				writer.append("</a>").append("<br/>");
 			}
 		}
 		catch(Exception ex)
@@ -58,27 +62,15 @@ public class DownloaderServlet extends HttpServlet {
 		}
 	}
 	
-	private void showFileContent(HttpServletResponse response, Path newPath) 
+	private void showFileContent(HttpServletResponse response, Path newPath)
 	{
-		try (ServletOutputStream servletOut = response.getOutputStream();
-				BufferedOutputStream bufferedOut = new BufferedOutputStream(servletOut);
-				FileInputStream fileIn = new FileInputStream(newPath.toFile());
-				BufferedInputStream bufferedIn = new BufferedInputStream(fileIn);) 
+		try{
+			response.getOutputStream().write(Files.readAllBytes(newPath));
+		}
+		catch(Exception e)
 		{
-			int size = fileIn.available();
-			while (size > 0) {
-				int bufSize = (size > PAGE_SIZE) ? PAGE_SIZE : size;
-				byte[] bytes = new byte[bufSize];
-				int read = bufferedIn.read(bytes, 0, bufSize);
-				if(read!=-1)
-				{
-					bufferedOut.write(bytes, 0, read);
-				}
-				size = fileIn.available();
 			
-			}
-		} catch (Exception ex) {
-		
 		}
 	}
+	
 }
